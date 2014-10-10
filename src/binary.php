8d6115675ee694ec357ea91cc30ffeef26677523
Steel Brain <aneesiqbalbhatti@gmail.com>
2014-10-10 10:26:14 +0500

    Change coding style of binary.php back to standard

--- src/binary.php
+++ src/binary.php
@@ -2,15 +2,12 @@
 
 namespace SocketIO;
 
-class Binary
-{
-     public function __construct($binarystring)
-     {
+class Binary{
+     public function __construct($binarystring){
           $this->binarystring = $binarystring;
      }
 
-     public function __toString()
-     {
+     public function __toString(){
           return $this->binarystring;
      }
 }
